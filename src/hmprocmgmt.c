830c4d12d819226a07a893ffac69a78b7e4b985b
Anshul Thakur <athakur@cdot.in>
2015-06-17 16:49:04 +0530

    Missed adding file to Repo

---
+++ src/hmprocmgmt.c
@@ -0,0 +1,144 @@
+/*
+ * hmprocmgmt.c
+ *
+ *  Created on: 15-Jun-2015
+ *      Author: anshul
+ */
+
+#include <hmincl.h>
+
+/***************************************************************************/
+/* Name:	hm_process_add 									*/
+/* Parameters: Input - 										*/
+/*			   Input/Output -								*/
+/* Return:	int32_t									*/
+/* Purpose: Adds the Process Control Block into its parent Node CB and 			*/
+/***************************************************************************/
+int32_t hm_process_add(HM_PROCESS_CB *proc_cb, HM_NODE_CB *node_cb)
+{
+	/***************************************************************************/
+	/* Variable Declarations												   */
+	/***************************************************************************/
+	int32_t ret_val = HM_OK;
+	HM_PROCESS_CB *insert_cb = NULL;
+	/***************************************************************************/
+	/* Sanity Checks														   */
+	/***************************************************************************/
+	TRACE_ENTRY();
+
+	TRACE_ASSERT(proc_cb != NULL);
+	TRACE_ASSERT(node_cb != NULL);
+
+	/***************************************************************************/
+	/* Main Routine															   */
+	/***************************************************************************/
+	proc_cb->parent_node_cb = node_cb;
+
+	/***************************************************************************/
+	/* Try insertion into the Node Process Tree.							   */
+	/* If a copy is found, verify that the first instance was not running.	   */
+	/* Now, there is one major worry with keeping old data marked as stale in  */
+	/* the Database, that being, if each node keeps on creating Processes and  */
+	/* destroying them, and each time a new instance is created, it is given a */
+	/* new PID. Then, the DB Trees here will get un-necessarily big with mostly*/
+	/* stale data. That is a condition of a very jittery system, though and is */
+	/* not expected. Hence, we are okay with it.							   */
+	/***************************************************************************/
+	insert_cb = (HM_PROCESS_CB *)HM_AVL3_INSERT_OR_FIND(
+								node_cb->process_tree,
+								proc_cb->node,
+								node_process_tree_by_proc_type_and_pid);
+	if(insert_cb != NULL)
+	{
+		TRACE_INFO(("Process entry already existed. Validate and Update"));
+		if(insert_cb->running == TRUE)
+		{
+			TRACE_WARN(("Previous Process was also running. This is erroneous!"));
+			ret_val = HM_ERR;
+			goto EXIT_LABEL;
+		}
+
+		/***************************************************************************/
+		/* Free the current Process CB											   */
+		/***************************************************************************/
+		TRACE_DETAIL(("Freeing new Process CB"));
+		hm_free_process_cb(proc_cb);
+		proc_cb = NULL;
+	}
+	else
+	{
+		/***************************************************************************/
+		/* Fixup pointers. We'll use insert_cb only from now on.				   */
+		/***************************************************************************/
+		insert_cb = proc_cb;
+	}
+	/***************************************************************************/
+	/* Inherit the role of its parent node.									   */
+	/***************************************************************************/
+	insert_cb->role = insert_cb->parent_node_cb->role;
+
+	/***************************************************************************/
+	/* Create a Global Process CB Entry										   */
+	/***************************************************************************/
+	if(hm_global_process_add(insert_cb)!= HM_OK)
+	{
+		TRACE_ERROR(("Error adding Process to Global Tables."));
+		HM_AVL3_DELETE(node_cb->process_tree, insert_cb->node);
+		ret_val = HM_ERR;
+		goto EXIT_LABEL;
+	}
+
+	/***************************************************************************/
+	/* Send an update of the process being added to subscribers				   */
+	/* Processes are not pre-provisioned. So, if this routine is being hit 	   */
+	/* means that process actually has been created, in that case, subscribers */
+	/* must know.															   */
+	/***************************************************************************/
+	hm_global_process_update(insert_cb);
+
+EXIT_LABEL:
+	/***************************************************************************/
+	/* Exit Level Checks													   */
+	/***************************************************************************/
+	TRACE_EXIT();
+	return (ret_val);
+}/* hm_process_add */
+
+/***************************************************************************/
+/* Name:	hm_process_update 									*/
+/* Parameters: Input - 										*/
+/*			   Input/Output -								*/
+/* Return:	int32_t									*/
+/* Purpose: Updates the state of Process and causes triggers.			*/
+/***************************************************************************/
+int32_t hm_process_update(HM_PROCESS_CB *proc_cb)
+{
+	/***************************************************************************/
+	/* Variable Declarations												   */
+	/***************************************************************************/
+	int32_t ret_val = HM_OK;
+	/***************************************************************************/
+	/* Sanity Checks														   */
+	/***************************************************************************/
+	TRACE_ENTRY();
+
+	/***************************************************************************/
+	/* Update the Global Tables												   */
+	/***************************************************************************/
+	hm_global_process_update(proc_cb);
+
+	/***************************************************************************/
+	/* TODO: UPDATE Interfaces tables too									   */
+	/***************************************************************************/
+
+	/***************************************************************************/
+	/* Main Routine															   */
+	/***************************************************************************/
+EXIT_LABEL:
+	/***************************************************************************/
+	/* Exit Level Checks													   */
+	/***************************************************************************/
+	TRACE_EXIT();
+	return(ret_val);
+}/* hm_process_update */
+
